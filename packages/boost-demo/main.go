067717d11ad0cf0ad376539d35a2deec052cfe43
George Knee <georgeknee@googlemail.com>
2023-07-05 11:44:05 +0100

    set CORS headers

--- packages/boost-demo/main.go
+++ packages/boost-demo/main.go
@@ -21,6 +21,15 @@ func main() {
 	}
 
 	http.HandleFunc("/nitro-protocol.pdf", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			_, _ = w.Write([]byte("OK"))
+			return
+		}
+
 		http.ServeContent(w, r, "nitro-protocol.pdf", time.Now(), io.NewSectionReader(bytes.NewReader(file), 0, int64(len(file))))
 	})
 
