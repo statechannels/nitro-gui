979850d22d5ba64e9e0cc4da668e836b3f41c149
George Knee <georgeknee@googlemail.com>
2023-07-05 13:51:03 +0100

    make it work

--- packages/boost-demo/src/App.tsx
+++ packages/boost-demo/src/App.tsx
@@ -5,41 +5,80 @@ import axios, { isAxiosError } from "axios";
 import "./App.css";
 
 function App() {
+  const numChunks = 100;
+  const length = 403507;
+
+  const [gotChunks, setGotChunks] = useState(new Array(numChunks).fill(false));
+
+  function setChunkGot(index: number) {
+    const gotChunksCopy = [...gotChunks];
+    gotChunksCopy[index] = true;
+    setGotChunks(gotChunksCopy);
+  }
+
   const [errorText, setErrorText] = useState<string>("");
 
-  const fetchFile = () => {
-    setErrorText("");
+  async function getChunk(index: number, file: Int8Array[]) {
+    if (index >= numChunks) {
+      throw Error;
+    }
+    const chunkSize = Math.floor(length / numChunks);
+    const startByte = index * chunkSize;
+    const endByte = Math.min(length, (index + 1) * chunkSize);
+    const range = "bytes=" + startByte + "-" + endByte;
 
-    axios
-      .get(`http://localhost:8081/nitro-protocol.pdf`, {
-        responseType: "blob", // This lets us download the file
-        headers: {
-          Accept: "*/*", // TODO: Do we need to specify this?
-        },
-      })
-
-      .then((result) => {
-        // This will prompt the browser to download the file
-        const blob = result.data;
-        const blobUrl = URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = blobUrl;
-        a.download = "fetched-file-from-ipfs";
-        a.click();
-        a.remove();
-        window.URL.revokeObjectURL(blobUrl);
-      })
-      .catch((e) => {
+    try {
+      const result = await axios.get(
+        `http://localhost:8081/nitro-protocol.pdf`,
+        {
+          responseType: "blob", // This lets us download the file
+          headers: {
+            Accept: "*/*", // TODO: Do we need to specify this?
+            Range: range,
+          },
+        }
+      );
+      setChunkGot(index);
+      file[index] = result.data;
+    } catch {
+      (e: any) => {
         if (isAxiosError(e)) {
           setErrorText(`${e.message}: ${e.response?.statusText}`);
         } else {
           setErrorText(JSON.stringify(e));
         }
-      });
+      };
+    }
+  }
+
+  function compileFile(file: Int8Array[]): File {
+    const f = new File(file, "nitro-protocol.pdf");
+    return f;
+  }
+
+  function triggerCompleteFileDownload(file: Int8Array[]) {
+    const fileUrl = URL.createObjectURL(compileFile(file));
+    const a = document.createElement("a");
+    a.href = fileUrl;
+    a.download = "nitro-protocol.pdf";
+    a.click();
+    a.remove();
+    window.URL.revokeObjectURL(fileUrl);
+  }
+
+  const fetchFile = async () => {
+    setErrorText("");
+    const file = new Array<Int8Array>(numChunks);
+    for (let i = 0; i < numChunks; i++) {
+      await getChunk(i, file);
+    }
+
+    triggerCompleteFileDownload(file);
   };
 
   return (
     <Box>
+      {gotChunks.map((c) => (c ? "X" : "_"))}
       <Button onClick={fetchFile}>Fetch</Button>
       <Box>{errorText}</Box>
     </Box>
